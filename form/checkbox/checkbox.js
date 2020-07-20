b9477c09eb52860a5bd58f725e5a7beb0c767e4b
licongp <licongp@kaisagroup.com>
2020-07-20 14:04:01 +0800

    add checkbox.js for test

---
+++ form/checkbox/checkbox.js
@@ -0,0 +1,52 @@
+Vue.component('light-checkbox', {
+    name: 'light-checkbox',
+    data: function(){
+        return {
+
+        }
+    },
+    props: {
+        model: String,
+        value: String
+    },
+    model: {
+        prop: 'model',
+        event: 'change'
+    },
+    computed: {
+        checked: function(){
+            return this.model === this.value
+        }
+    },
+    methods: {
+        handleChange: function(e){
+            let checked = e.target.checked;
+            checked && this.$emit('change', this.value);
+        }
+    },
+
+    template: `
+        <label 
+          class="light-checkbox"
+          :class="[
+              {'is-checked':checked}
+          ]"
+        >
+            <span class="light-radio_input">
+                <span class="light-radio_inner">
+                </span>
+                <input
+                  type="radio" 
+                  :value="value"
+                  :checked="checked"
+                  v-on:change="handleChange"
+                  class="light-radio_original" 
+                />
+            </span>
+            <span class="light-radio_label">
+                <slot></slot>
+            </span>
+        </label>
+    `
+
+});
